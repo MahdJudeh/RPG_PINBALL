cf2c4171f555a16c32fb78b9e5d9ad1846e1db9a
Mahdi Judeh <mjudeh1@lsu.edu>
2017-02-05 23:04:16 -0600

    Added a 3d project type instead instead

---
+++ RPG_Pinball/Assets/Scripts/PaddleControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleControl : MonoBehaviour {
+	JointMotor2D motorA;
+	JointMotor2D motorB;
+	JointMotor2D motorReturnA;
+	JointMotor2D motorReturnB;
+	// Use this for initialization
+	void Start () 
+	{
+		motorA = gameObject.GetComponent<HingeJoint2D> ().motor;
+		motorB = gameObject.GetComponent<HingeJoint2D> ().motor;
+		motorReturnA = gameObject.GetComponent<HingeJoint2D> ().motor;
+		motorReturnB = gameObject.GetComponent<HingeJoint2D> ().motor;
+		motorA.motorSpeed = -1500;
+		motorB.motorSpeed = 1500;
+		motorReturnB.motorSpeed = -1000;
+		motorReturnA.motorSpeed = 1000;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if (gameObject.tag == "PaddleL") {
+			
+			if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) {
+				gameObject.GetComponent<HingeJoint2D> ().motor = motorA;
+			}
+			else{
+				gameObject.GetComponent<HingeJoint2D> ().motor = motorReturnA;
+			}
+		}
+		if (gameObject.tag == "PaddleR") {
+			if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) {
+				gameObject.GetComponent<HingeJoint2D> ().motor = motorB;
+			}
+			else{
+				gameObject.GetComponent<HingeJoint2D> ().motor = motorReturnB;
+			}
+		}
+	}
+}
